f94c5e23a00c6f6edd0f5f6c24f13e22be9ba217
Onur Eker <yonureker@gmail.com>
2017-12-03 18:43:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board(type, location)
+  cell = "   "
+  seperator = "|"
+  row_lines = "-----------"
+
+  row_0 = cell + seperator + cell + seperator + cell
+  row_1 = row_lines
+  row_2 = cell + seperator + cell + seperator + cell
+  row_3 = row_lines
+  row_4 = cell + seperator + cell + seperator + cell
+
+  puts row_0
+  puts row_1
+  puts row_2
+  puts row_3
+  puts row_4
+end
